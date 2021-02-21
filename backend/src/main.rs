4935f6ef2953426300fa0bd2d72340207190b232
Aleksander Vognild Burkow <aleksanderburkow@gmail.com>
2021-02-21 17:55:29 +0100

    Update cargo backend

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,27 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use rocket_contrib::json::Json;
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Status {
+    pub clans: Vec<String>,
+    pub prize_pool: u64,
+}
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Json<Status> {
+    Json(Status {
+        clans: vec![
+            "FaZe Clan".to_string(),
+            "Sølvguttene".to_string(),
+            "VUKUKARSK".to_string(),
+        ],
+        prize_pool: 2_000,
+    })
 }
 
 fn main() {
